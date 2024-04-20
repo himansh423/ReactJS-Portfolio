70d58197cc95ca7ba027ff5375960d945f41f9c7
Himanshu Chauhan <himanshuchau423@gmail.com>
2024-04-20 16:59:23 +0530

    deploying

--- PortFolio/src/components/Detail.jsx
+++ PortFolio/src/components/Detail.jsx
@@ -4,7 +4,7 @@ const Detail = () => {
     <div className={styles.parent}>
       <p className={styles.name}>Hi, I'm Himanshu!</p>
       <div className={styles.container}>
-        <p className={styles.description}>I'm a Frontend <span className={styles.colorOfDev}>Developer</span>, pursuing BCA in <span className={styles.colorOfsci}>Data Science</span>.</p>
+        <p className={styles.description}>I'm a Full-Stack<span className={styles.colorOfDev}> Developer</span>, pursuing BCA in <span className={styles.colorOfsci}>Data Science</span>.</p>
       </div>
     </div>
   );
